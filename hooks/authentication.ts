265de4dbbe0ac85658e113ebfb5dba59c1a98daa
insideplant <kami.10pearl@gmail.com>
2022-01-23 22:57:18 +0900

    finish by 09

--- hooks/authentication.ts
+++ hooks/authentication.ts
@@ -54,6 +54,7 @@ export function useAuthentication() {
         const loginUser: User = {
           uid: firebaseUser.uid,
           isAnonymous: firebaseUser.isAnonymous,
+          name: ''
         };
         setUser(loginUser);
         createUserIfNotFound(loginUser);
