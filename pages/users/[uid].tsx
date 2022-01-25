3677637627f86112f654a3808c42b18fe3ebd59d
insideplant <kami.10pearl@gmail.com>
2022-01-25 23:55:11 +0900

    add Bootstrap

--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -1,16 +1,46 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { User } from "../../models/User";
 import { useRouter } from "next/router";
-import { collection, doc, getDoc, getFirestore } from "firebase/firestore";
+import Layout from "../../components/Layout";
+import { useAuthentication } from "../../hooks/authentication";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getFirestore,
+  serverTimestamp,
+} from 'firebase/firestore'
 
 type Query = {
   uid: string;
 };
 
+
+
 export default function UserShow() {
   const [user, setUser] = useState<User>(null);
   const router = useRouter();
   const query = router.query as Query;
+  const { user: currentUser } = useAuthentication();
+  const [body, setBody] = useState("");
+
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+  
+    const db = getFirestore()
+  
+    await addDoc(collection(db, 'questions'), {
+      senderUid: currentUser.uid,
+      receiverUid: user.uid,
+      body,
+      isReplied: false,
+      createdAt: serverTimestamp(),
+    })
+  
+    setBody('')
+    alert('質問を送信しました。')
+  }
 
   useEffect(() => {
     if (query.uid === undefined) {
@@ -32,5 +62,35 @@ export default function UserShow() {
     loadUser();
   }, [query.uid]);
 
-  return <div>{user ? user.name : "ロード中…"}</div>;
+  return (
+    <Layout>
+      <div className="container">
+        {user && (
+          <div className="text-center">
+            <h1 className="h4">{user.name}さんのページ</h1>
+            <div className="m-5">{user.name}さんに質問しよう！</div>
+            <div className="row justify-content-center mb-3">
+              <div className="col-12 col-md-6">
+                <form onSubmit={onSubmit}>
+                  <textarea
+                    className="form-control"
+                    placeholder="おげんきですか？"
+                    rows={6}
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    required
+                  ></textarea>
+                  <div className="m-3">
+                    <button type="submit" className="btn btn-primary">
+                      質問を送信する
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </Layout>
+  );
 }
